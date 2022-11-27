f91f331703744eaeb336a926dd1dac8167198619
Denis Gichana <denisgichana162@gmail.com>
2022-11-27 23:19:06 +0300

    add versions

--- src/Components/ProjectManager/Files/DesignFiles.jsx
+++ src/Components/ProjectManager/Files/DesignFiles.jsx
@@ -1,10 +1,14 @@
-import React, { Fragment, useState } from "react";
-import { useSelector } from "react-redux";
+import React, { Fragment, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import CreateDesignFile from "../../CreateProject/CreateDesignFile/CreateDesignFile";
 import "./style.css";
 import { BsCloudUploadFill } from "react-icons/bs";
 import Versions from "./Versions/Versions";
+import {
+  createVersion,
+  fetchVersions,
+} from "../../../redux/actions/versionActions";
 
 function DesignFiles({ project }) {
   const [display, setDisplay] = useState(false);
@@ -12,6 +16,9 @@ function DesignFiles({ project }) {
   const [file, setFile] = useState("");
   const { name, projectName } = useParams();
   const { design__files } = useSelector((state) => state.designFileReducer);
+  // const { project } = useSelector((state) => state.projectReducer);
+  const { versionNumber } = useSelector((state) => state.versionReducer);
+  const dispatch = useDispatch();
   console.log(design__files);
   const handleDisplay = (e) => {
     e.preventDefault();
@@ -21,18 +28,30 @@ function DesignFiles({ project }) {
     e.preventDefault();
     setVersion((prevVersion) => !prevVersion);
   };
+
   const handleUpload = (e) => {
     e.preventDefault();
+    const versionFile = {
+      version: versionNumber,
+      fileName: e.target.filenames[0].name,
+      designFile: project.projectName,
+    };
+    dispatch(createVersion(versionFile));
   };
+  useEffect(() => {
+    project && dispatch(fetchVersions(project.projectName));
+  }, [project]);
   return (
     <div className="design__files">
       <div className="add__file">
+        <h1>{project.projectName}</h1>
         <h4 className="h4__add__file" onClick={(e) => handleDisplay(e)}>
           Click To Add File
         </h4>
         {display && <CreateDesignFile />}
       </div>
-      {design__files &&
+      {project &&
+        design__files &&
         design__files?.map((file, index) => {
           return (
             <Fragment key={index}>
