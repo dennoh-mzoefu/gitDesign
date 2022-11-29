9bc973cda42c2934fc97d087e3dc33d689913aae
Denis Gichana <denisgichana162@gmail.com>
2022-11-29 12:22:10 +0300

    add comoponent

--- src/Components/ProjectManager/Files/Versions/Versions.jsx
+++ src/Components/ProjectManager/Files/Versions/Versions.jsx
@@ -1,34 +1,32 @@
 import React from "react";
+import { BiGitCompare } from "react-icons/bi";
+import { useSelector } from "react-redux";
 import "./style.css";
-
 function Versions() {
+  const { versions } = useSelector((state) => state.versionReducer);
+  console.log({ versions });
   return (
-    <div className="versions">
-      <div className="section full-height">
-        <input
-          className="modal-btn"
-          type="checkbox"
-          id="modal-btn"
-          name="modal-btn"
-        />
-        {/* <label for="modal-btn">
-          Open Modal <i className="uil uil-expand-arrows"></i>
-        </label> */}
-        <div className="modal">
-          <div className="modal-wrap">
-            <img src="https://assets.codepen.io/1462889/sl3.jpg" alt="" />
-            <p>
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC,
-              making it over 2000 years old.
-            </p>
-          </div>
-        </div>
-
-        {/* <a href="https://front.codes/" className="logo" target="_blank">
-          <img src="https://assets.codepen.io/1462889/fcy.png" alt="" />
-        </a> */}
-      </div>
+    <div className="version__container">
+      {/* hello againa agiamn */}
+      {versions &&
+        versions?.map((version, index) => {
+          return (
+            <div className="version__div" key={index}>
+              <img
+                className="version__image"
+                src={`${window.location.origin}/images/file/files/${version.fileName}`}
+              />
+              {index !== 0 && <BiGitCompare className="compare" />}
+              {index === 0 && <BiGitCompare className="compare__0" />}
+              {index !== 0 && (
+                <img
+                  className="version__image"
+                  src={`${window.location.origin}/images/file/differences/${version.fileName}`}
+                />
+              )}
+            </div>
+          );
+        })}
     </div>
   );
 }
