1eb58398eb15a0bb319342626ccfe5b07d30b6bb
Denis Gichana <denisgichana162@gmail.com>
2022-11-29 12:26:27 +0300

    add redux

--- src/redux/actions/versionActions.js
+++ src/redux/actions/versionActions.js
@@ -2,9 +2,10 @@ import { FETCH__VERSION, CREATE__VERSION, ERROR } from "./types";
 import * as api from "../api/index.js";
 
 export const fetchVersions = (designFile) => async (dispatch) => {
+  console.log({ designFile });
   try {
     const { data } = await api.fetchVersions(designFile);
-
+    console.log({ data });
     dispatch({ type: FETCH__VERSION, payload: data });
   } catch (error) {
     dispatch({ type: ERROR, payload: error.message });
@@ -15,7 +16,8 @@ export const fetchVersions = (designFile) => async (dispatch) => {
 export const createVersion = (versionFile) => async (dispatch) => {
   try {
     const { data } = await api.createVersion(versionFile);
-
+    console.log(versionFile);
+    console.log({ data });
     dispatch({ type: CREATE__VERSION, payload: data });
   } catch (error) {
     dispatch({ type: ERROR, payload: error.message });
