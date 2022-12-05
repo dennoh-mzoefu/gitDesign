c7fde26db5917455ea0b0fd3daa837ca063fc12b
Denis Gichana <denisgichana162@gmail.com>
2022-12-05 16:42:01 +0300

    wip

--- src/Components/ProjectManager/Files/Versions/AboveVersions.jsx
+++ src/Components/ProjectManager/Files/Versions/AboveVersions.jsx
@@ -3,14 +3,14 @@ import { useDispatch } from "react-redux";
 import { fetchVersions } from "../../../../redux/actions/versionActions";
 import Versions from "./Versions";
 
-function AboveVersions({ designFile }) {
+function AboveVersions({ designFile, setVersion }) {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchVersions(designFile));
   }, [designFile]);
   return (
     <div>
-      <Versions />
+      <Versions setVersion={setVersion} />
     </div>
   );
 }
