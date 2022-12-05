122121a3f44b0804244c91044da0c3138c56b47f
Denis Gichana <denisgichana162@gmail.com>
2022-12-05 16:35:10 +0300

    wip

--- src/redux/actions/activityLogActions.js
+++ src/redux/actions/activityLogActions.js
@@ -1,15 +1,23 @@
-import {
-    FETCH__ACTIVITY__LOGS,ERROR
-  } from "./types";
-  import * as api from "../api/index.js";
+import { FETCH__ACTIVITY__LOGS, ERROR, CREATE__ACTIVITY__LOG } from "./types";
+import * as api from "../api/index.js";
 
-  export const getActivityLogs = () => async (dispatch) => {
-    try {
-      const { data } = await api.fetchActivityLogs();
-  
-      dispatch({ type: FETCH__ACTIVITY__LOGS, payload: data });
-    } catch (error) {
-      dispatch({ type: ERROR, payload: error.message });
-      // console.log(error.message);
-    }
-  };
+export const getActivityLogs = () => async (dispatch) => {
+  try {
+    const { data } = await api.fetchActivityLogs();
+
+    dispatch({ type: FETCH__ACTIVITY__LOGS, payload: data });
+  } catch (error) {
+    dispatch({ type: ERROR, payload: error.message });
+    // console.log(error.message);
+  }
+};
+export const saveActivityLog = (data) => async (dispatch) => {
+  try {
+    const { data } = await api.createActivityLog(data);
+
+    dispatch({ type: CREATE__ACTIVITY__LOG, payload: data });
+  } catch (error) {
+    dispatch({ type: ERROR, payload: error.message });
+    // console.log(error.message);
+  }
+};
